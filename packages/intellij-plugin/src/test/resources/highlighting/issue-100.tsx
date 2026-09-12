5bbac1bab75f23cf569236c5840cd69c19f8eaaf
Jon K. Wheeler <jon.k.wheeler@gmail.com>
2026-09-12 12:37:24 -0400

    Address PR review feedback (#101)
    - avoid the key probe's fixture identifier collision

--- packages/intellij-plugin/src/test/resources/highlighting/issue-100.tsx
+++ packages/intellij-plugin/src/test/resources/highlighting/issue-100.tsx
@@ -1,5 +1,5 @@
 const list = <ul title={viewModel.title} />;
 const count = <p>{visibleItems.length}</p>;
 const items = <p>{visibleItems.map(renderItem)}</p>;
-const keyed = <li key={item.text}>{item.label}</li>;
+const row = <li key={item.text}>{item.label}</li>;
 const empty = <li className="no-todos">No todos</li>;
